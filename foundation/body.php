93e2da1a6debac53ae9873dccc0a63d55faf63f7
Santeri Hukari <santeri.hukari2@kpedu.fi>
2015-08-13 14:29:13 +0300

    foundation-kansio lisatty

---
+++ foundation/body.php
@@ -0,0 +1,38 @@
+<div class="row">
+  <div class="large-12 columns">
+    
+      <nav class="top-bar" data-topbar>
+        <ul class="title-area">
+                   
+          <li class="name">
+            <h1>
+            <a href="#">
+            Trafi projekti
+            </a>
+            </h1>
+          </li>
+          <li class="toggle-topbar menu-icon"><a href="#"><span>menu</span></a></li>
+          </ul>
+             
+          <section class="top-bar-section">
+          </section>
+      </nav>
+               
+    </div>
+  <div class="row">
+    <div class="large-12 columns">
+      <div class="panel">
+        <div class="row">
+     
+          <div class="large-12 small-12 columns" id="hakukone">
+            <strong>Hakukone<hr/></strong>
+          </div>
+     
+        </div>
+      </div>
+    </div>
+     
+         
+     
+  </div>
+</div>
