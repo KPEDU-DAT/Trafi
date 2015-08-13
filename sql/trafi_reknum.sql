cb8e8587fe94b55b988c7f63af0d8de9178b15b6
thelewa <leevi.juusela@kpedu.fi>
2015-08-13 13:27:46 +0300

    siirsin tiedoston

--- sql/trafi_reknum.sql
+++ sql/trafi_reknum.sql
@@ -1,40 +1,37 @@
--- MySQL dump 10.14  Distrib 5.5.44-MariaDB, for debian-linux-gnu (i686)
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: data14
--- ------------------------------------------------------
--- Server version	5.5.44-MariaDB-1ubuntu0.14.04.1
+-- Palvelin: localhost
+-- Luontiaika: 13.08.2015 klo 13:22
+-- Palvelimen versio: 5.5.44-MariaDB-1ubuntu0.14.04.1
+-- PHP:n versio: 5.5.9-1ubuntu4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `trafi_reknum`
+-- Tietokanta: `data14`
 --
 
-DROP TABLE IF EXISTS `trafi_reknum`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `trafi_reknum` (
+-- --------------------------------------------------------
+
+--
+-- Rakenne taululle `trafi_reknum`
+--
+
+CREATE TABLE IF NOT EXISTS `trafi_reknum` (
   `valmistenumero2` int(10) NOT NULL,
-  `rekisterinumero` varchar(7) COLLATE utf8_swedish_ci NOT NULL
+  `rekisterinumero` varchar(7) COLLATE utf8_swedish_ci NOT NULL,
+  PRIMARY KEY (`valmistenumero2`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2015-08-13 12:35:18
