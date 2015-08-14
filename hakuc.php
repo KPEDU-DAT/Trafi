c480c4605ec893ec1b7929bb057f0d6c41241a61
Jumalaa <patrik.sipi@kpedu.fi>
2015-08-14 08:46:24 +0300

    Täydellinen versio ei tarvitse muokata

--- hakuc.php
+++ hakuc.php
@@ -6,22 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Tavoite 3</title>
 
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/~patriksipi/Foundation/bower_components/foundation/css/foundation.css"/>
+	<script src="/~patriksipi/Foundation/bower_components/modernizr/modernizr.js"></script>
 
-    <!--[if lt IE 9]>
-      <script src="http://oss.maxcdn.com/html15shiv/3.7.2/html15shiv.min.js"></script>
-      <script src="http://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
   <body>
-    <div class="container">
-    <div class="jumbotron">
-    <h1>Palkkatiedot</h1>
+    <h1>Ajoneuvotiedot</h1>
         <p>
-            <form action="tavoite2_tun.php" method="POST">
-                <p>Anna sosiaaliturvatunnus:
-                <input type="text" name="sotu">
-                <p><button class="btn btn-info btn-lg" type="submit" name="laheta" value="true">Lähetä</button>
+            <form action="hakuc.php" method="POST">
+                <p>Anna VIN-koodi:
+                <input type="text" name="valmistenumero2">
+                <p><button class="btn btn-info btn-lg" type="submit" name="laheta" value="true">Hae</button>
             </form>
         </p>
         <p>
@@ -32,15 +27,14 @@
             }
             $yhteys->set_charset('utf8');
 
-            if ($_POST['sotu']) {
-                $sotu = mysqli_real_escape_string($yhteys, $_POST['sotu']);
-                $tulos = mysqli_query($yhteys, "SELECT nimet.etunimi, nimet.sukunimi, tyontekijat.palkka 
-                                                FROM nimet, tyontekijat 
-                                                WHERE nimet.sotu = tyontekijat.id 
-                                                AND nimet.sotu = '" . $sotu . "'");
+            if ($_POST['valmistenumero2']) {
+                $VIN = mysqli_real_escape_string($yhteys, $_POST['valmistenumero2']);
+                $tulos = mysqli_query($yhteys, "SELECT *
+                                                FROM trafi_ajoneuvot
+                                                WHERE trafi_ajoneuvot.valmistenumero2 = '" . $VIN . "'");
 
                 while ($rivi = mysqli_fetch_array($tulos)) {
-            echo $rivi["sukunimi"] . " " . $rivi["etunimi"] . ", " . $rivi["palkka"] . " €/kk";
+            echo $rivi["ajoneuvoluokka"] . " " . $rivi["ensirekisterointipvm"] . ", " . $rivi["ajoneuvoryhma"] . " " . $rivi["ajoneuvonkaytto"] . " " . $rivi["variantti"]. " " . $rivi["versio"]. " " . $rivi["kayttoonottopvm"]. " " . $rivi["vari"]. " " . $rivi["ovienlukumaara"]. " " . $rivi["korityyppi"]. " " . $rivi["ohjaamotyyppi"]. " " . $rivi["istumapaikkojenlkm"]. " " . $rivi["omamassa"]. " " . $rivi["teknSuurSallKokmassa"]. " " . $rivi["tieliikSuurSallKokmassa"]. " " . $rivi["ajonKokPituus"]. " " . $rivi["ajonLeveys"]. " " . $rivi["ajonKorkeus"]. " " . $rivi["Kayttovoima"]. " " . $rivi["iskutilavuus"]. " " . $rivi["suurinNettoteho"]. " " . $rivi["sylintereidenLkm"]. " " . $rivi["ahdin"]. " " . $rivi["sahkohybridi"]. " " . $rivi["merkkiSelvakielinen"]. " " . $rivi["mallimerkinta"]. " " . $rivi["vaihteisto"]. " " . $rivi["vaihteidenlkm"]. " " . $rivi["kaupallinenNimi"]. " " . $rivi["voimanValJaTehostamistapa"]. " " . $rivi["tyyppihyvaksynta"]. " " . $rivi["yksittaisKayttovoima"]. " " . $rivi["kunta"]. " " . $rivi["Co2"]. " " . $rivi["mittarilukema"]. " " . $rivi["alue"];
 
                 }
             }
@@ -49,9 +43,26 @@
 
         ?>
         </p>
-    </div>
-    </div>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <script>
+  document.write('<script src=js/vendor/' +
+  ('__proto__' in {} ? 'zepto' : 'jquery') +
+  '.js><\/script>')
+  </script>
+	<script src="http://foundation.zurb.com/assets/js/jquery.js"></script>
+	<script src="http://foundation.zurb.com/templates/js/foundation.min.js"></script>
+	<script>
+    	$(document).foundation();
+  	</script>
+	<script src="http://foundation.zurb.com/assets/js/templates/jquery.js"></script>
+	<script src="http://foundation.zurb.com/assets/js/templates/foundation.js"></script>
+	<script>
+    	  $(document).foundation();
+    	  var doc = document.documentElement;
+    	  doc.setAttribute('data-useragent', navigator.userAgent);
+    </script>
+
+
+</body>
+</html>
   </body>
 </html>
