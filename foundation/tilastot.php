9273ef995465550428d870f7c4a3774997ada870
jsnstd <jsngit@gmail.com>
2015-08-17 10:07:41 +0100

    .

--- foundation/tilastot.php
+++ foundation/tilastot.php
@@ -18,13 +18,13 @@
     <? include("ypalkki.php") ?>                           
     <div class="row" style="height: 100px;"></div>      
     <div class="row">
-      <div class="small-6 medium-6 large-6 small-centered medium-centered large-centered columns">
+      <div class="small-6 medium-6 large-6 columns">
         <table>
         <th>a</th>
         <td>a</td>
         </table>
       </div>
-      <div class="small-6 medium-6 large-6 small-centered medium-centered large-centered columns">
+      <div class="small-6 medium-6 large-6 columns">
         <table>
         <th>a</th>
         <td>a</td>
