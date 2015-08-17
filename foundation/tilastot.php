bbc0d782d2f82fedcea8e5558dadc2b930bed4c7
jsnstd <jsngit@gmail.com>
2015-08-17 09:27:23 +0100

    .

---
+++ foundation/tilastot.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <?php
+
+        $yht = mysqli_connect("85.157.249.252", "data14", "mv2Mqbm888DvqbjT", "data14");
+        echo mysqli_error($yht);
+
+    ?>
+
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Foundation</title>
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+
+  <body>                                                             
+    <? include("ypalkki.php") ?>                           
+    <div class="row" style="height: 100px;"></div>      
+    <div class="row">
+      <div class="small-6 medium-6 large-6 small-centered medium-centered large-centered columns">
+        test
+      </div>
+	</div>
+  </body>
+  <? include("footer.php"); ?>
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+</html>
