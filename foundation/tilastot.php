b846128bde28747053514a21ee98d0bc9edb3129
jsnstd <jsngit@gmail.com>
2015-08-17 10:47:04 +0100

    .

--- foundation/tilastot.php
+++ foundation/tilastot.php
@@ -31,7 +31,8 @@
                     $res = mysqli_query($yht, "SELECT merkkiSelvakielinen, COUNT(merkkiSelvakielinen) from trafi_ajoneuvot GROUP BY merkkiSelvakielinen ORDER BY COUNT(merkkiSelvakielinen) DESC LIMIT 10;");
                     while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
                         echo "<tr><td>" . $row['merkkiSelvakielinen']. "</td><td>" . $row['count(merkkiselvakielinen)'] . "</td></tr>";
-                    }
+                        print_r($row);
+}
                 ?>
         </tbody>
         </table>
