fe99b4d7e7f1383b297d59a470fb68c881a5308c
Janne Nyman <janne.nyman@kpedu.fi>
2015-08-18 10:13:28 +0300

    ääkköset

--- foundation/tekijat.php
+++ foundation/tekijat.php
@@ -10,10 +10,10 @@
 <div class="row">
   <div class="large-12 columns">
       <div class="large-6 columns" id="rtext">
-        <p>Hakukone a by Janne, Mathias, Erkka, Björk and Alex.</p>
-        <p>Hakukone b by Joona, Juho, Hannu, Miro and Erika.</p>
-        <p>Hakukone c by Patric, Jori, Leevi, Antti and joku</p>
-        <p>Integrointi by Santeri and Janne and Thylin.</p>
+        <p><b>Hakukone A </b>&middot  Erkka and Alex.</p>
+        <p><b>Hakukone B </b>&middot Joona, Juho, Hannu, Miro and Erika.</p>
+        <p><b>Hakukone C </b>&middot Patric, Jori, Leevi, Antti and joku</p>
+        <p><b>Integrointi </b>&middot Santeri, Janne and Thylin.</p>
       </div>
     </div>
   </div>
