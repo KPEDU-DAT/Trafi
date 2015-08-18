cf2b90bd894831f5fe477a2386dc19fc4e292c8b
Janne Nyman <janne.nyman@kpedu.fi>
2015-08-18 08:57:47 +0300

    muutoksia

--- foundation/footer.php
+++ foundation/footer.php
@@ -3,10 +3,10 @@
     <div class="row">
 
       <div class="large-6 columns" id="footertext">
-        <p>Hakukone a by Janne, Mathias, Erkka, Björk and Alex.</p>
+        <p>Hakukone a by Janne, Mathias, Erkka, Börk and Alex.</p>
         <p>Hakukone b by Joona, Juho, Hannu, Miro and Erika.</p>
-        <p>Hakukone c by Janne, Jori, Leevi, Antti and joku</p>
-        <p>Integrointi by Santeri and Jesse.</p>
+        <p>Hakukone c by Patric, Jori, Leevi, Antti and joku</p>
+        <p>Integrointi by Santeri and Janne and Thylin.</p>
         <p>© Copyright no one at all. Go home.</p>
       </div>
 
--- foundation/ypalkki.php
+++ foundation/ypalkki.php
@@ -3,7 +3,7 @@
   <section class="top-bar-section">
     <!-- Right Nav Section -->
     <ul class="right">
-      <li><a href="body.php">Index</a></li>
+      <li><a href="index.php">Index</a></li>
       <li><a href="habody.php">Hakukone A</a></li>
       <li><a href="hbbody.php">Hakukone B</a></li>
       <li><a href="hcbody.php">Hakukone C</a></li>
