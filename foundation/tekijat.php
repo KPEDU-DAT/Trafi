1f61fc22bd23fe34e4912bf3f71dda746b93d147
mathiasthylin <mathias.thylin@kpedu.fi>
2015-08-18 10:05:03 +0300

    muutoksia

--- foundation/tekijat.php
+++ foundation/tekijat.php
@@ -1,39 +1,22 @@
+
+
 <body>
-  <? include("ypalkki.php") ?>
-  <div class="row"></div>
-    <div class=" small-3 medium-3 large-3 small-centered medium-centered
-large-centered columns">
-      <? include("auto.php"); ?>
-    </div>
-  <div class="row">
-    <div class="small-6 medium-6 large-6 small-centered medium-centered
-large-centered columns">
-
-    </div>
-  </div>
-<footer class="row">
-  <div class="large-12 columns"><hr/>
-    <div class="row">
-
-      <div class="large-6 columns" id="footertext">
+  <? include("body.php") ?>   
+<div class="row">
+  <div class="large-12 columns">
+      <div class="large-6 columns" id="rtext">
         <p>Hakukone a by Janne, Mathias, Erkka, Börk and Alex.</p>
         <p>Hakukone b by Joona, Juho, Hannu, Miro and Erika.</p>
         <p>Hakukone c by Patric, Jori, Leevi, Antti and joku</p>
         <p>Integrointi by Santeri and Janne and Thylin.</p>
-        <p>© Copyright no one at all. Go home.</p>
       </div>
-
-      <div class="large-6 columns">
-        <ul class="inline-list right">
-          <li><a href="#">Home</a></li>
-        </ul>
-      </div>
-
     </div>
   </div>
-</footer>
 </body>
-<? include("footer.php"); ?>
+
+
+
+
 
 
 
