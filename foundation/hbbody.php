74d1abef88ed5de7bc7005eee2108e233e7807d4
Janne Nyman <janne.nyman@kpedu.fi>
2015-08-19 13:08:27 +0300

    fnej

--- foundation/hbbody.php
+++ foundation/hbbody.php
@@ -8,23 +8,19 @@
   </head>
 	
 	<body>                                                                      
-	  <? include("navbar.php") ?> 
+	  <? include("body.php") ?> 
 	  <div class="row" style="height: 100px;"></div>
 	  <div class="row">
 	    <div class="small-3 medium-3 large-3 small-centered medium-centered large-centered columns">
-	       <? include("auto.php"); ?>
 	       </div>
         </div>
     <div class="row">
       <div class="small-6 medium-6 large-6 small-centered medium-centered large-centered columns">	     
-	     
-	     
-	      <? include("hakubuusi1.php"); ?>                
+        <? include("hakubuusi1.php"); ?>                
         </div>                                        
       </div>
       </div>      
     </body>                        
-<? include("footer.php"); ?>
     <script src="js/vendor/jquery.js"></script>
     <script src="js/foundation.min.js"></script>
     <script>
