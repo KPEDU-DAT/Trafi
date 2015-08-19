21568004a3abb467c1f2e4a53106783aaf66e7a7
Janne Nyman <janne.nyman@kpedu.fi>
2015-08-19 13:10:48 +0300

    fne

--- foundation/hcbody.php
+++ foundation/hcbody.php
@@ -8,20 +8,13 @@
   </head>
 	
 	<body>                                                                      
-	  <? include("navbar.php") ?> 
-	  <div class="row" style="height: 100px;"></div>
-	  <div class="row">
-	    <div class="small-3 medium-3 large-3 small-centered medium-centered large-centered columns">
-	      <? include("auto.php"); ?>
-        </div>                                        
-      </div>
+	  <? include("body.php") ?> 
       <div class="row">
         <div class="small-6 medium-6 large-6 small-centered medium-centered large-centered columns">
           <? include("hakuc1.php"); ?>
         </div>
       </div>      
     </body>                        
-<? include("footer.php"); ?>
     <script src="js/vendor/jquery.js"></script>
     <script src="js/foundation.min.js"></script>
     <script>
