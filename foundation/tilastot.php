225140916390f8f31ccc976a29f667b4e2aea3f5
jsnstd <jsngit@gmail.com>
2015-08-19 13:33:49 +0300

    .

--- foundation/tilastot.php
+++ foundation/tilastot.php
@@ -15,7 +15,7 @@
   </head>
 
   <body>                                                             
-    <? include("ypalkki.php") ?>                           
+    <? include("navbar.php") ?>                           
     <div class="row" style="height: 100px;"></div>      
     <div class="row">
     <div class="small-6 medium-6 large-6 columns">
