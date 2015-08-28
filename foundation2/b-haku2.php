7990132a1cddb6ce15e4edaf3c9790a3272361e8
Juhiofi <juho.petajisto@kpedu.fi>
2015-08-28 08:28:18 +0300

    haun pilkkomista

---
+++ foundation2/b-haku2.php
@@ -0,0 +1,54 @@
+      <p>Valitse haluamasi malli: </p>
+      <select name="malli">
+
+      <option disabled value="desc1">Toyota</option>
+      <option value="AVENSIS">Avensis</option>
+      <option value="STARLET">Starlet</option>
+      <option value="HIACE">Hiace</option>
+
+      <option disabled value="desc2">Volkswagen</option>
+      <option value="GOLF">Golf</option>
+      <option value="PASSAT">Passat</option>
+      <option value="TRANSPORTER_1.9TD-70X0A-K">Transporter
+      1.9TD-70X0A-K</option>
+
+      <option disabled value="desc3"><u>Mercedes-Benz</u></option>
+      <option value="C_200_CDI">C 200 CDI</option>
+      <option value="E_200_CDI">E 200 CDI</option>
+      <option value="E_200_CDI">E 220 CDI</option>
+
+      <option disabled value="desc4">Audi</option>
+      <option value="A4">A4</option>
+      <option value="A6">A6</option>
+
+      <option disabled value="desc5">Saab</option>
+      <option value="9-3">9-3</option>
+      <option value="900">900</option>
+
+      <option disabled value="desc6">Renault</option>
+      <option value="MEGANE">Megane</option>
+
+      <option disabled value="desc6">Ford</option>
+      <option value="FOCUS">Focus</option>
+      <option value="MONDEA">Mondea</option>
+      <option value="TRANSIT_100">Transit 100</option>
+
+      <option disabled value="desc7">Scoda</option>
+      <option value="OCTAVIA">Ocativia</option>
+
+      <option disabled value="desc8">Mazda</option>
+      <option value="6">6</option>
+      <option disabled value="desc9">BMW</option>
+      <option value="530d">530d</option>
+      <option value="523i">523i</option>
+
+      <option disabled value="desc10">Volvo</option>
+      <option value="240">240</option>
+      <option value="V70">v70</option>
+
+      <option disabled value="desc11">Citroen</option>
+      <option value="XANTIA">Xantia</option>
+
+      <option disabled value="desc12">Mitsubishi</option>
+      <option value="PAJERO">Pajero</option>
+    </select>
