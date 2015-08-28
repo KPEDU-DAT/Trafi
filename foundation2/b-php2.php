7ac6efe4d2e79cd9214213bc6024785c7efbc68f
Hannu-pekka <hannu-pekka.lundstrom@kpedu.fi>
2015-08-28 09:14:51 +0300

    päivityksiä

--- foundation2/b-php2.php
+++ foundation2/b-php2.php
@@ -1,4 +1,6 @@
 <?php
+$X=$_POST['merkki'];
+$Y=$_POST['vuosi'];
 $yhteys=mysqli_connect("localhost","data14","mv2Mqbm888DvqbjT","data14");
 if(mysqli_connect_errno())
 {
@@ -9,6 +11,7 @@ $tulos=mysqli_query($yhteys,"SELECT trafi_ajoneuvot.mallimerkinta FROM trafi_ajo
                             WHERE trafi_ajoneuvot.ensirekisterointipvm LIKE '".$Y."%'
                             AND trafi_ajoneuvot.merkkiSelvakielinen LIKE '".$X."';" );
 
-
+while($rivi=mysqli_fetch_array($tulos))	{
+  echo $rivi['mallimerkinta']." ";
 mysqli_close($yhteys);
 ?>
