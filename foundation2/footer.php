18617ce94bd2aa904d0cb02cd0fd2fc9a8fae585
Janne Nyman <janne.nyman@kpedu.fi>
2015-08-28 10:32:57 +0300

    tyyliejä

--- foundation2/footer.php
+++ foundation2/footer.php
@@ -1,13 +1,4 @@
 <!-- FOOTER -->
-      <style>
-      footer {
-    position: fixed;
-    bottom: 0;
-    height: 50px;
-    width: 90%;
-    overflow:hidden;
-        }
-      </style>
       <footer>
         <p>&copy; 2015 13Dat. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a> &middot; <a href="tekijat.php">Tekijät</a></p>
       </footer>
