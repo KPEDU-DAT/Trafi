f983ace3101992769b28a07bd8e8ac23cda4cbbc
Juhiofi <juho.petajisto@kpedu.fi>
2015-08-28 08:26:40 +0300

    B-haun korjausta

--- foundation2/b-haku.php
+++ foundation2/b-haku.php
@@ -17,60 +17,6 @@
       <option value="Mitsubishi">Mitsubishi</option>
       <option value="BMW">BMW</option>
       </select>
-      <p>Valitse haluamasi malli: </p>
-      <select name="malli">
-
-      <option disabled value="desc1">Toyota</option>
-      <option value="AVENSIS">Avensis</option>
-      <option value="STARLET">Starlet</option>
-      <option value="HIACE">Hiace</option>
-
-      <option disabled value="desc2">Volkswagen</option>
-      <option value="GOLF">Golf</option>
-      <option value="PASSAT">Passat</option>
-      <option value="TRANSPORTER_1.9TD-70X0A-K">Transporter
-      1.9TD-70X0A-K</option>
-
-      <option disabled value="desc3"><u>Mercedes-Benz</u></option>
-      <option value="C_200_CDI">C 200 CDI</option>
-      <option value="E_200_CDI">E 200 CDI</option>
-      <option value="E_200_CDI">E 220 CDI</option>
-
-      <option disabled value="desc4">Audi</option>
-      <option value="A4">A4</option>
-      <option value="A6">A6</option>
-
-      <option disabled value="desc5">Saab</option>
-      <option value="9-3">9-3</option>
-      <option value="900">900</option>
-
-      <option disabled value="desc6">Renault</option>
-      <option value="MEGANE">Megane</option>
-
-      <option disabled value="desc6">Ford</option>
-      <option value="FOCUS">Focus</option>
-      <option value="MONDEA">Mondea</option>
-      <option value="TRANSIT_100">Transit 100</option>
-
-      <option disabled value="desc7">Scoda</option>
-      <option value="OCTAVIA">Ocativia</option>
-
-      <option disabled value="desc8">Mazda</option>
-      <option value="6">6</option>
-      <option disabled value="desc9">BMW</option>
-      <option value="530d">530d</option>
-      <option value="523i">523i</option>
-
-      <option disabled value="desc10">Volvo</option>
-      <option value="240">240</option>
-      <option value="V70">v70</option>
-
-      <option disabled value="desc11">Citroen</option>
-      <option value="XANTIA">Xantia</option>
-
-      <option disabled value="desc12">Mitsubishi</option>
-      <option value="PAJERO">Pajero</option>
-    </select>
 
       <p>Valitse haluttu vuosimalli: </p>
 
@@ -103,49 +49,7 @@
 
     <table class="table">
 
-<?php
-$X = $_POST['merkki'];
-$Z = $_POST['malli'];
-$Y = $_POST['vuosi'];
-$btn = $_POST['nappi'];
-if($btn=="True")
-{
-$yhteys=mysqli_connect("localhost","data14","mv2Mqbm888DvqbjT","data14");
-if(mysqli_connect_errno())
-{
-die("MySQL, virhe yhteyden luonnissa: ".mysqli_connect_error());
-}
-$yhteys->set_charset('utf8');
-$tulos=mysqli_query($yhteys,"SELECT * FROM trafi_ajoneuvot
-                            WHERE trafi_ajoneuvot.ensirekisterointipvm LIKE
-'".$Y."%'
-                            AND trafi_ajoneuvot.merkkiSelvakielinen LIKE
-'".$X."'
-                            AND trafi_ajoneuvot.mallimerkinta LIKE
-'%".$Z."%';" );
-    echo "<table>";
-    echo  "<tr><th>Luokka</th><th>1. rekisteröinti
-pvm</th><th>Ryhmä</th><th>Versio</th><th>Ovien Lkm</th><th>Korityyppi</th><th>Merkki</th><th>Malli</th><th>Co2 päästöt</th></tr>";
-while($rivi=mysqli_fetch_array($tulos)) {
-    echo "<tr>
-   	    
-<td>".$rivi['ajoneuvoluokka']."</td>
-<td>".$rivi['ensirekisterointipvm']."</td>
-<td>".$rivi['ajoneuvoryhma']."</td>
-<td>".$rivi['versio']."</td>
-<td>".$rivi['ovienlukumaara']."</td> 
-<td>".$rivi['korityyppi']."</td>
-<td>".$rivi['merkkiSelvakielinen']."</td>
-<td>".$rivi['kaupallinenNimi']."</td>
-<td>".$rivi['Co2']."</td></tr>";
-  }
-  echo "</table>";
-mysqli_close($yhteys);
-}
-else
-{
-echo "";
-}
+
 
 ?>
     </p>
