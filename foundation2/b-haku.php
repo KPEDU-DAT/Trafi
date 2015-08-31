3fc8eb4f90bb954a2e12433d756ebac1e78f7040
Juhiofi <juho.petajisto@kpedu.fi>
2015-08-31 09:04:20 +0300

    asd

--- foundation2/b-haku.php
+++ foundation2/b-haku.php
@@ -1,48 +1,48 @@
-    <form methodd="POST" action="b-body.php">
-     <p>Valitse haluamasi merkki: </p>
-      <select name="merkki">
-        <option value="Audi">Audi</option>
-        <option value="BMW">BMW</option>
-        <option value="Citroen">Citroen</option>
-        <option value="Ford">Ford</option>
-        <option value="Mazda">Mazda</option>
-        <option value="Mercedes-Benz">Mercedes-Benz</option>
-        <option value="Mitsubishi">Mitsubishi</option>
-        <option value="Renault">Renault</option>
-        <option value="Saab">Saab</option>
-        <option value="Skoda">Skoda</option>
-        <option value="Toyota">Toyota</option>
-        <option value="Volkswagen">Volkswagen</option>
-        <option value="Volvo">Volvo</option>
-	  </select>
-	    <p>  Valitse haluttu vuosimalli: </p>
-    	  <select name="vuosi">
-      		<option value="1990">1990</option>
-      		<option value="1991">1991</option>
-      		<option value="1992">1992</option>
-    	    <option value="1993">1993</option>
-      		<option value="1994">1994</option>
-      		<option value="1995">1995</option>
-      		<option value="1996">1996</option>
-      		<option value="1997">1997</option>
-      		<option value="1998">1998</option>
-      		<option value="1999">1999</option>
-      		<option value="2000">2000</option>
-      		<option value="2001">2001</option>
-      		<option value="2002">2002</option>
-      		<option value="2003">2003</option>
-      		<option value="2004">2004</option>
-      		<option value="2005">2005</option>
-      		<option value="2006">2006</option>
-      		<option value="2007">2007</option>
-      		<option value="2008">2008</option>
-      		<option value="2009">2009</option>
-      		<option value="2010">2010</option>
-      		<option value="2011">2011</option>
-      	  </select>
-          <? include("b-php2.php"); ?>
-      <button name="button" value="true" type="submit"> Seuraava</button>
-    </form>
+<form method="POST" action="b-body.php">
+  <p>Valitse haluamasi merkki: </p>
+    <select name="merkki">
+      <option value="Audi">Audi</option>
+      <option value="BMW">BMW</option>
+      <option value="Citroen">Citroen</option>
+      <option value="Ford">Ford</option>
+      <option value="Mazda">Mazda</option>
+      <option value="Mercedes-Benz">Mercedes-Benz</option>
+      <option value="Mitsubishi">Mitsubishi</option>
+      <option value="Renault">Renault</option>
+      <option value="Saab">Saab</option>
+      <option value="Skoda">Skoda</option>
+      <option value="Toyota">Toyota</option>
+      <option value="Volkswagen">Volkswagen</option>
+      <option value="Volvo">Volvo</option>
+    </select>
+      <p>  Valitse haluttu vuosimalli: </p>
+    <select name="vuosi">
+      <option value="1990">1990</option>
+      <option value="1991">1991</option>
+      <option value="1992">1992</option>
+      <option value="1993">1993</option>
+      <option value="1994">1994</option>
+      <option value="1995">1995</option>
+      <option value="1996">1996</option>
+      <option value="1997">1997</option>
+      <option value="1998">1998</option>
+      <option value="1999">1999</option>
+      <option value="2000">2000</option>
+      <option value="2001">2001</option>
+      <option value="2002">2002</option>
+      <option value="2003">2003</option>
+      <option value="2004">2004</option>
+      <option value="2005">2005</option>
+      <option value="2006">2006</option>
+      <option value="2007">2007</option>
+      <option value="2008">2008</option>
+      <option value="2009">2009</option>
+      <option value="2010">2010</option>
+      <option value="2011">2011</option>
+    </select>
+  <? include("b-php2.php"); ?>
+  <button name="button" value="true" type="submit"> Seuraava</button>
+</form>
 <? echo $X." ".$Y." "; ?>
 <script>
       document.write('<script src=js/vendor/' +
