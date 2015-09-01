130f69a96f8c093124b423e3081a0367a7d41a77
Janne Nyman <janne.nyman@kpedu.fi>
2015-09-01 10:43:20 +0300

    tilastot päivitys

--- foundation2/tilastot.php
+++ foundation2/tilastot.php
@@ -36,7 +36,10 @@
   </head>
 
   <body>
-       <div class="row"></div>
+  <? include("nav.php"); ?>
+  <? include("auto.php"); ?>
+  
+         <div class="row"></div>
       <div class=" small-3 medium-3 large-3 small-centered medium-centered large-centered columns">
    
       </div>
@@ -72,6 +75,7 @@
         </table>
     </div>
     </div>
+    
   </body>
   <? include("footer.php"); ?>
 
