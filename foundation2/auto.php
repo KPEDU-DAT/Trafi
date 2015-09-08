a94739ab0b36c87c658626ce7aacc6da16f3a978
mathiasthylin <mathias.thylin@kpedu.fi>
2015-09-08 11:16:33 +0300

    auto

--- foundation2/auto.php
+++ foundation2/auto.php
@@ -1,5 +1,5 @@
 <div class="small-3 medium-3 large-3 small-centered medium-centered large-centered columns">
   <div class="img">
-    <a href="index.php"><img src="http://static.ipaustralia.com.au/store3/6/75/675219.1.high.jpg"></a>
+    <a href="index.php"><img src="audi.jpg"></a>
   </div>
 </div>
