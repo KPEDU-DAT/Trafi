e002af85d127a5099da25523b8659fe358bc97c5
Janne Nyman <janne.nyman@kpedu.fi>
2015-09-08 10:53:26 +0300

    footer

--- foundation2/footer.php
+++ foundation2/footer.php
@@ -1,6 +1,6 @@
 <!-- FOOTER -->
-<footer>
+<div id="clearfooter"></div>
   <div class="footer">
     <p>&copy; 2015 13Dat. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a> &middot; <a href="tekijat.php">Tekijät</a></p>
   </div>      
-</footer>
+
