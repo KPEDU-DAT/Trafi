b4b08f4a44998b252e1a26e68365d3aa3de5a054
Janne Nyman <janne.nyman@kpedu.fi>
2015-09-09 08:42:44 +0300

    navia

--- foundation2/navuusi2.php
+++ foundation2/navuusi2.php
@@ -1,6 +1,7 @@
-<div class="row">
+<div class="icon-bar three-up">
   <dl class="sub-nav">
     <dd><a href="hakua_2.php">REK-haku</a></dd>
     <dd><a href="b-haku.php">Ajoneuvo-haku</a></dd>
     <dd><a href="hakuc_2.php">VIN-haku</a></dd>
   </dl>
+</div>
